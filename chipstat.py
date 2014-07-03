329570c27d7fc3f3fa8ad1aff67abf87b475f316
Jonas Minnberg <sasq64@gmail.com>
2014-07-03 09:11:33 +0200

    Added get_list

--- chipstat.py
+++ chipstat.py
@@ -5,6 +5,7 @@ from google.appengine.ext import ndb
 
 class SongPlayedData(ndb.Model):
 	path = ndb.StringProperty(indexed=False)
+	collection = ndb.StringProperty(indexed=False)
 	date = ndb.DateTimeProperty(auto_now_add=True)
 
 
@@ -17,6 +18,20 @@ class MainPage(webapp2.RequestHandler):
 		for s in songdata:
 			self.response.write(s.path + "\n") 
 
+class GetList(webapp2.RequestHandler):
+	def get(self):
+		#p = self.request.body
+		#data = json.decode(p)
+		q = SongPlayedData.query(ancestor=ndb.Key('SongsPlayed', 'default')).order(-SongPlayedData.date)
+		songdata = q.fetch(100)
+
+		self.response.headers['Content-Type'] = 'application/json'
+		plist = []
+		for s in songdata:
+			plist.append({ 'path' : s.path, 'collection' : s.collection })
+		self.response.write(json.encode(plist))
+
+
 class SongPlayed(webapp2.RequestHandler):
 	def post(self):
 		p = self.request.body
@@ -27,14 +42,16 @@ class SongPlayed(webapp2.RequestHandler):
 		spd = SongPlayedData(parent=ndb.Key('SongsPlayed', 'default'))
 		logging.info('Key: ' + str(spd.key))
 		spd.path = data['path']
+		spd.collection = data['collection']
 		spd.put()
 
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write(p)
+		self.response.headers['Content-Type'] = 'application/json'
+		self.response.write(json.encode("OK"))
 
 
 
 application = webapp2.WSGIApplication([
 	('/', MainPage),
 	('/song_played', SongPlayed),
+	('/get_list', GetList),
 ], debug=True)
